43532af6b8988d0ab04d65947a2e9c747342c33b
f0rd2017 <101038483+f0rd2017@users.noreply.github.com>
2022-04-15 12:04:13 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function IdiNah(){
+
+    var newDiv = document.createElement("div");
+
+
+
+    newDiv.innerHTML = "<span style=\"font-size:45px\" id=\"SpanHui\">ИДИ НАХУЙ ДУРА ЕБАНАЯ<span><br>";
+
+
+
+    my_div = document.getElementById("IdiNahui");
+
+    my_div.appendChild(newDiv);
+}
